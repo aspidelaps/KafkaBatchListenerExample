4ea7ad1d72f1fc8cc503fee15a68e302e5a46119
aspidelaps <b.g.markar@gmail.com>
2026-06-19 16:50:34 +0300

    FINALLY TRACING WORKS

--- src/test/java/containers/KafkaTestContainer.java
+++ src/test/java/containers/KafkaTestContainer.java
@@ -10,13 +10,11 @@ import org.testcontainers.utility.DockerImageName;
 @Testcontainers
 public class KafkaTestContainer {
 
-    // Define the Kafka container using a specific Docker image
     @Container
     public static KafkaContainer kafkaContainer = new KafkaContainer(
             DockerImageName.parse("apache/kafka-native:3.8.0")
     );
 
-    // Dynamically set the Kafka bootstrap servers property in the Spring context
     @DynamicPropertySource
     public static void dynamicProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
